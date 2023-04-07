169fd470ac92596e9882164139dc3a332a552b4d
viki2207 <114282438+viki2207@users.noreply.github.com>
2023-04-07 19:26:37 +1000

    Update app.js

--- app.js
+++ app.js
@@ -11,10 +11,7 @@ res.setHeader("Access-Control-Allow-Credentials", true);
     res.send("API is running..");
 });
 app.use("/books", router); //localhost:5030/books
-mongoose.connect("mongodb+srv://root:root@cluster0.f8bifn6.mongodb.net/bookStore?retryWrites=true&w=majority",{
-      useNewUrlParser: true,
-      useUnifiedTopology: true,
-    }).then(() => console.log("Connected to database"))
+mongoose.connect("mongodb+srv://root:root@cluster0.f8bifn6.mongodb.net/bookStore?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }).then(() => console.log("Connected to database"))
   .then(() => {
     app.listen(5030);
   })
