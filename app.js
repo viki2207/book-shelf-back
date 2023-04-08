faf20cf2642f36478e6af5df717323faa6d4e6ae
viki2207 <114282438+viki2207@users.noreply.github.com>
2023-04-08 11:16:12 +1000

    Update app.js

--- app.js
+++ app.js
@@ -3,7 +3,6 @@ const mongoose = require("mongoose");
 const router = require("./routes/book-routes");
 const cors = require("cors");
 const app = express();
-const {ServerApiVersion} = require("mongodb");
 //Middlewares
 app.use(express.json());
 app.use(cors());
@@ -12,7 +11,7 @@ res.setHeader("Access-Control-Allow-Credentials", true);
     res.send("API is running..");
 });
 app.use("/books", router); //localhost:5030/books
-mongoose.connect("mongodb+srv://root:root@cluster0.f8bifn6.mongodb.net/bookStore?retryWrites=true&w=majority",{ useNewUrlParser: true, useUnifiedTopology: true, serverApi: ServerApiVersion.v1 }).then(() => console.log("Connected to database"))
+mongoose.connect("mongodb+srv://root:root@cluster0.f8bifn6.mongodb.net/bookStore?retryWrites=true&w=majority").then(() => console.log("Connected to database"))
   .then(() => {
     app.listen(5030);
   })
